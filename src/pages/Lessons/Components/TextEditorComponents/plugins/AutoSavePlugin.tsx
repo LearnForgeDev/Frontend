99901d08cfac1821d902e973b6eb36bd5ec3f2bc
StasWW <01102006stas@gmail.com>
2026-01-23 23:02:54 +0300

    feat: added saving editor state to the server

--- src/pages/Lessons/Components/TextEditorComponents/plugins/AutoSavePlugin.tsx
+++ src/pages/Lessons/Components/TextEditorComponents/plugins/AutoSavePlugin.tsx
@@ -1,11 +1,13 @@
 import {type JSX, useEffect} from "react";
 import {useLexicalComposerContext} from "@lexical/react/LexicalComposerContext";
 import {serializedDocumentFromEditorState} from "@lexical/file";
+import {sendEditorStateAsJson} from "../../../../../server/Lessons.ts";
 
 export default function AutoSavePlugin({lessonId}: {lessonId: number | string}): JSX.Element | null {
   const [editor] = useLexicalComposerContext();
 
-  const SAVE_INTERVAL_MS = 30_000;
+  const SAVE_LOCALLY_INTERVAL_MS = 30_000;
+  const SAVE_TO_SERVER_INTERVAL_MS = 15 * 60_000;
 
   useEffect(() => {
     const saveEditor = setInterval(() => {
@@ -13,10 +15,21 @@ export default function AutoSavePlugin({lessonId}: {lessonId: number | string}):
         JSON.stringify(serializedDocumentFromEditorState(editor.getEditorState()));
 
       sessionStorage.setItem(`lesson-draft-${lessonId}`, savedEditorState);
-    }, SAVE_INTERVAL_MS)
+    }, SAVE_LOCALLY_INTERVAL_MS)
 
     return () => clearInterval(saveEditor);
   }, [editor, lessonId]);
+
+  useEffect(() => {
+    const saveEditorToServer = setInterval(() => {
+        sendEditorStateAsJson(
+          lessonId,
+          serializedDocumentFromEditorState(editor.getEditorState()),
+        );
+    }, SAVE_TO_SERVER_INTERVAL_MS);
+
+    return () => clearInterval(saveEditorToServer);
+  }, [SAVE_TO_SERVER_INTERVAL_MS, editor, lessonId]);
 
   return null;
 }
