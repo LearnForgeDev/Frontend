bf83feb2022e46c0d58d35c0ac5846b7db19e131
StasWW <01102006stas@gmail.com>
2026-06-14 21:23:10 +0300

    chore: remove path prefix from base URL and update server deployment command

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ createRoot(document.getElementById('root')!).render(
       <AppGlobalStyles />
       <QueryClientProvider client={queryClient}>
         <UserProvider>
-          <BrowserRouter basename='/Frontend'>
+          <BrowserRouter>
             <AppRoutes />
           </BrowserRouter>
         </UserProvider>
